3ce7e245ef4adb7e65584e51158b535dd86e575c
CITIZENDOT <appaji12368@gmail.com>
2020-12-18 21:51:58 +0530

    Fixed Hard-coded version

--- pyintelowl/cli/_utils.py
+++ pyintelowl/cli/_utils.py
@@ -110,6 +110,6 @@ def get_json_data(filepath):
 
 
 def get_version_number() -> str:
-    # f = pathlib.Path(__file__).joinpath("../../../version.txt").resolve()
-    # return f.read_text()
-    return "3.0.0"
+    from .. import version
+
+    return version.__version__
