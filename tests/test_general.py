357d636c2d930686a3ac66e8b4f731e8054fec34
Sarthak Khattar <sarthakoct@gmail.com>
2021-07-27 15:04:15 +0530

    add more general tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -3,6 +3,7 @@ from .utils import (
     BaseTest,
     mock_connections,
     patch,
+    get_test_file_data,
 )
 from .mocked_requests import (
     mocked_analyzer_config,
@@ -19,3 +20,53 @@ class TestGeneral(BaseTest):
     @mock_connections(patch("requests.Session.get", side_effect=mocked_raise_exception))
     def test_get_analyzer_config_failure(self, mocked_requests):
         self.assertRaises(IntelOwlClientException, self.client.get_analyzer_configs)
+
+    def test_get_md5_observable(self):
+        type_ = "observable"
+        test_string = "test"
+        hashed_value = self.client.get_md5(test_string, type_)
+        self.assertEqual(hashed_value, "098f6bcd4621d373cade4e832627b4f6")
+
+    def test_get_md5_binary(self):
+        type_ = "binary"
+        test_string = get_test_file_data(self.filepath)
+        hashed_value = self.client.get_md5(test_string, type_)
+        self.assertEqual(hashed_value, "c35736429b30030acb60cb19c771649e")
+
+    def test_get_md5_file_success(self):
+        type_ = "file"
+        test_string = self.filepath
+        hashed_value = self.client.get_md5(test_string, type_)
+        self.assertEqual(hashed_value, "c35736429b30030acb60cb19c771649e")
+
+    def test_get_md5_file_failure(self):
+        type_ = "file"
+        test_string = "/path/to/non-existent/file"
+        self.assertRaises(
+            IntelOwlClientException, self.client.get_md5, test_string, type_
+        )
+
+    def test__get_observable_classification_ip(self):
+        test_value = self.ip
+        result = self.client._get_observable_classification(test_value)
+        self.assertEqual(result, "ip")
+
+    def test__get_observable_classification_domain(self):
+        test_value = self.domain
+        result = self.client._get_observable_classification(test_value)
+        self.assertEqual(result, "domain")
+
+    def test__get_observable_classification_url(self):
+        test_value = self.url
+        result = self.client._get_observable_classification(test_value)
+        self.assertEqual(result, "url")
+
+    def test__get_observable_classification_hash(self):
+        test_value = self.hash
+        result = self.client._get_observable_classification(test_value)
+        self.assertEqual(result, "hash")
+
+    def test__get_observable_classification_generic(self):
+        test_value = self.generic
+        result = self.client._get_observable_classification(test_value)
+        self.assertEqual(result, "generic")
--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,21 @@
 import json
+import pathlib
 from unittest import TestCase
 from unittest.mock import patch  # noqa: F401
 
 from pyintelowl.pyintelowl import IntelOwl
-from tests import MOCK_CONNECTIONS
+from tests import (
+    MOCK_CONNECTIONS,
+    TEST_IP,
+    TEST_DOMAIN,
+    TEST_URL,
+    TEST_HASH,
+    TEST_GENERIC,
+    TEST_FILE,
+    TEST_FILE_HASH,
+)
+
+ROOT_DIR = pathlib.Path(__file__).parent.parent
 
 
 # class for mocking responses
@@ -26,11 +38,21 @@ class MockResponse:
 class BaseTest(TestCase):
     def setUp(self):
         self.client = IntelOwl("test-token", "test-url")
+        self.ip = TEST_IP
+        self.url = TEST_URL
+        self.domain = TEST_DOMAIN
+        self.hash = TEST_HASH
+        self.generic = TEST_GENERIC
+        self.filepath = f"{ROOT_DIR}/test_files/{TEST_FILE}"
+        self.file_hash = TEST_FILE_HASH
 
 
 def mock_connections(decorator):
     return decorator if MOCK_CONNECTIONS else lambda x: x
 
 
-def mocked_requests(*args, **kwargs):
-    return MockResponse({}, 200)
+def get_test_file_data(filepath):
+    with open(filepath, "rb") as f:
+        file = f.read()
+
+    return file
