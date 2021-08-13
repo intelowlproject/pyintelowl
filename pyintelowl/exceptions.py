ff86144fdc85a79ceefad8bf1edbd052eff73a51
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-08-13 16:37:39 +0530

    refactor IntelOwl class and exception

--- pyintelowl/exceptions.py
+++ pyintelowl/exceptions.py
@@ -1,6 +1,25 @@
-class IntelOwlClientException(Exception):
-    pass
+import json
+import typing
+from requests.exceptions import RequestException
 
 
-class IntelOwlInvalidAPITokenException(Exception):
-    pass
+class IntelOwlClientException(RequestException):
+    @property
+    def error_detail(self) -> typing.Union[typing.Dict, typing.AnyStr]:
+        content = None
+        try:
+            content = self.response.json()
+            detail = content.get("detail", None)
+            if detail:
+                content = detail
+        except json.JSONDecodeError:
+            content = self.response.content
+        except Exception:
+            pass
+
+        return content
+
+    def __str__(self):
+        err_msg = super().__str__()
+        detail = self.error_detail
+        return err_msg + f". Details: {detail}"
