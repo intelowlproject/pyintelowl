6f5bc8ed906932ce43a19203f2c2b813b0676ed5
0x0elliot <wolfwithahat@protonmail.com>
2022-09-30 04:10:46 +0530

    Fixing playbook test cases

--- tests/test_general.py
+++ tests/test_general.py
@@ -186,13 +186,12 @@ class TestGeneral(BaseTest):
         observable_name = self.domain
         runtime_config = {"test_key": "test_param"}
         playbooks_requested = ["TEST_PLAYBOOK"]
-        result_ = self.client.send_observable_analysis_playbook_request(
+        result = self.client.send_observable_analysis_playbook_request(
             observable_name,
             playbooks_requested=playbooks_requested,
             runtime_configuration=runtime_config,
         )
 
-        result = result_.get("results")
         self.assertIn("status", result)
         self.assertIn("job_id", result)
         self.assertIn("analyzers_running", result)
@@ -207,13 +206,12 @@ class TestGeneral(BaseTest):
         filename = self.filepath
         binary = get_file_data(self.filepath)
         runtime_config = {"test_key": "test_param"}
-        result_ = self.client.send_file_analysis_playbook_request(
+        result = self.client.send_file_analysis_playbook_request(
             filename,
             binary,
             playbooks_requested=playbooks_requested,
             runtime_configuration=runtime_config,
         )
-        result = result_.get("results")
 
         self.assertIn("status", result)
         self.assertIn("job_id", result)
