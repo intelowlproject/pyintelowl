0317afef9a4e1477942edadb8f312f3b9d79b17b
Federico Gibertoni <f.gibertoni@certego.net>
2024-11-15 10:23:56 +0100

    Removed unused options and fixed printing problem

--- pyintelowl/cli/investigations.py
+++ pyintelowl/cli/investigations.py
@@ -145,28 +145,11 @@ def _display_investigation_tree(data):
     help="Tabular print investigation attributes and results for an investigation ID"
 )
 @click.argument("investigation_id", type=int)
-@click.option(
-    "-r",
-    "--report",
-    type=click.Choice(
-        [
-            "analyzers",
-            "connectors",
-        ]
-    ),
-    default="analyzers",
-    show_choices=True,
-    help="""
-    Choose the type of report to be displayed:
-    analyzer or connector.
-    """,
-)
 @add_options(json_flag_option)
 @click.pass_context
 def view(
     ctx: ClickContext,
     investigation_id: int,
-    report: str,
     as_json: bool,
 ):
     ctx.obj.logger.info(
@@ -187,28 +170,11 @@ def view(
     help="Tabular print investigation's tree structure for an investigation ID"
 )
 @click.argument("investigation_id", type=int)
-@click.option(
-    "-r",
-    "--report",
-    type=click.Choice(
-        [
-            "analyzers",
-            "connectors",
-        ]
-    ),
-    default="analyzers",
-    show_choices=True,
-    help="""
-    Choose the type of report to be displayed:
-    analyzer or connector.
-    """,
-)
 @add_options(json_flag_option)
 @click.pass_context
 def view_tree(
     ctx: ClickContext,
     investigation_id: int,
-    report: str,
     as_json: bool,
 ):
     ctx.obj.logger.info(
@@ -244,9 +210,9 @@ def ls(ctx: ClickContext, status: str, as_json: bool):
         results = ans.get("results", [])
         ctx.obj.logger.info(results)
         if status:
-            ans = [el for el in results if el["status"].lower() == status.lower()]
+            results = [el for el in results if el["status"].lower() == status.lower()]
         if as_json:
-            rprint(json.dumps(ans, indent=4))
+            rprint(json.dumps(results, indent=4))
         else:
             _display_all_investigations(ctx.obj.logger, results)
     except IntelOwlClientException as e:
