b345f1412d945cd171c72fe2e4e5a4a922149ba9
pranjalg1331 <pranjaloff13@gmail.com>
2025-01-01 14:34:47 +0530

    black rearranged
    Signed-off-by: pranjalg1331 <pranjaloff13@gmail.com>

--- pyintelowl/cli/_jobs_utils.py
+++ pyintelowl/cli/_jobs_utils.py
@@ -123,7 +123,9 @@ def _display_all_jobs(logger, rows):
                 ", ".join([t["label"] for t in el["tags"]]),
                 ", ".join(el["analyzers_to_execute"]),
                 ", ".join(el["connectors_to_execute"]),
-                ", ".join(el["playbook_to_execute"] if el["playbook_to_execute"] else []),
+                ", ".join(
+                    el["playbook_to_execute"] if el["playbook_to_execute"] else []
+                ),
                 str(el["process_time"]),
                 get_status_text(el["status"]),
             )
